881339d482ce5f80fd1e559a275020dc2899b3bb
Denis Urakanov <mcquicklyss@gmail.com>
2019-08-27 11:31:05 +0500

    Skeleton Webpack

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+const sayHello = name => console.log(`hello ${name}`);
+
+sayHello('Denis');
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    main: './src/js/main.js',
+    //about: './js/about.new.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+     contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'})
+  ]
+}
